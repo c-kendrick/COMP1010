82a72f878fcaad198d10b68cfa150cbf80c9b341
4liet <168448986+4liet@users.noreply.github.com>
2025-05-08 12:05:15 +1000

    Create equipment.java

---
+++ equipment.java
@@ -0,0 +1,19 @@
+
+public class equipment {
+    String name;
+    int health;
+    int strength;
+    int defence;
+    int initiative;
+    int type;
+    
+    public equipment(String name, int health, int strength, int defence, int initiative, int type) {
+        this.name = name;
+        this.health= health;
+        this.strength = strength;
+        this.defence = defence;
+        this.initiative = initiative;
+        this.type = type;
+    }
+
+}
