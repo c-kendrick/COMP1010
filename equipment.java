a34a76c6f0c98f2a3b54c09df3855b44ea0703d0
4liet <168448986+4liet@users.noreply.github.com>
2025-05-08 12:32:26 +1000

    Update equipment.java

--- equipment.java
+++ equipment.java
@@ -8,6 +8,9 @@ public class equipment {
     int type;
     
     public equipment(String name, int health, int strength, int defence, int initiative, int type) {
+        if (type < 1 || type > 4) {
+            throw new IllegalArgumentException("Type must be between 1 and 4 inclusive.");
+        }
         this.name = name;
         this.health= health;
         this.strength = strength;
@@ -15,5 +18,26 @@ public class equipment {
         this.initiative = initiative;
         this.type = type;
     }
-
+    public String typeName(int type){
+        String typeName="";
+        if (type == 1 ){
+            typeName = "helmet";
+        }
+        if (type == 2 ){
+            typeName = "Armour";
+        }
+        if (type == 3 ){
+            typeName = "Item";
+        }
+        if (type == 4 ){
+            typeName = "wepon";
+        }
+        return typeName;
+    }
+    
+    
+    
+    public String toString(){   
+        return "Equipment{" +"Name= " + name + ", Health= " + health + ", Strength= " + strength +", Defence= " + defence +", Initiative= " + initiative + ", Type= " + typeName(type) +'}';
+    }
 }
