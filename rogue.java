52599487c345ecfaa8a3be64a8695702fde624de
Patelephone <max.patel@students.mq.edu.au>
2025-05-26 02:29:40 +1000

    Update rogue.java

--- rogue.java
+++ rogue.java
@@ -1,42 +1,62 @@
 public class rogue extends Character {
     
     boolean isInvisible;
+    boolean hasStolenSpellBook;
 
 	public rogue(String r) {
 		health = 100;
 		damage = 15;
 		race = r;
+        intelligence = 5
+        initiative = 10
+        isInvisable = false 
+        hasStolenSpellBook = false 
 	}
 	
+    public rogue(String race, int health, int damage, int intelligence, int initiative) {
+        this.race = race;
+        this.health = health;
+        this.damage = damage;
+        this.intelligence = intelligence;
+        this.initiative = initiative; 
+
+
+
 	void attack (Character target){
         
         if (target instanceof engineer) {
-			target.takeDamage 
+			 if (deployedDevice && builtDevice.equals("WALL") && rog.isInvisible;
+             return; 
+
+        } else {
+            target.takeDamage; 
+        }
  }
 
         if (target instanceof mage) {
-			target.takeDamage 
+			target.takeDamgae;
         }
 
         if (target instanceof barbarian) {
-			target.takeDamage 
+			target.takeDamage;  
         }
 
 }
 
 
-void trackerDeployed() {
-    deployedDevice = true;
-    builtDevice = "TRACKER";
-    
-  
-    if (rog.isInvisible) {
-        rog.isInvisible = false;
-    
+
+@Override
+void specialAbility(Character target){ 
+    isInvisible = true 
+
+    if (target instanceof mage && isInvisable) {
+        mage m = (mage) target 
+        if (m.hasSpellBook);
+        m.loseSpellBook();
+        hasStolenSpellBook = true 
+        System.println("Rogue has stolen the Mage's Spell Book")
     }
 }
-
-//DO IF BARBARIAN RAGES 
 
 void takeDamage(int damage) {
     if 
