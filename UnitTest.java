eb471992ad7b61b2c053e0b1152ce7fb30cf8477
4liet <168448986+4liet@users.noreply.github.com>
2025-05-26 13:40:32 +1000

    unit test

---
+++ UnitTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UnitTest {
+    @Test
+    public void testFail() {
+        assertEquals(1, 2); // this should fail
+    }
+    @Test
+    public void testEquipment() {
+        equipment test = new equipment(6, "test", 10, 20, 0, 50, 4);
+        assertEquals(6, test.ID);  
+    }
+}
